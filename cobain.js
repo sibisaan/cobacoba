aab85e013550610749a4fa8e454d9005cd60f1f4
sibisaan <goestiancompany@gmail.com>
2024-11-26 01:09:04 +0700

    cobain.js

---
+++ cobain.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+// Membuat transporter menggunakan Gmail SMTP
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'dadangstoreegg@gmail.com',  // Ganti dengan alamat email Anda
+        pass: 'hzdv toub ewev zskz'                 // Ganti dengan password email Anda
+    }
+});
+
+// Opsi email
+let mailOptions = {
+    from: 'donotreply@moonton.com',  // Alamat email pengirim
+    to: 'donebank2000@gmail.com',    // Alamat email penerima
+    subject: 'Test Email',
+    text: 'This is a test email.'    // Isi email
+};
+
+// Kirim email
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.log('Error:', error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
